6736eb3a3b51498007957db779b3323c64a8cf0f
kaysmart001 <luwoyekayode99@gmail.com>
2023-02-07 09:37:32 +0100

    Update connect.php

--- faculty/connect.php
+++ faculty/connect.php
@@ -1,11 +1,11 @@
 <?php
 	
-$link = mysql_connect('localhost', 'root', 'b0kubh00t');
+$link = mysql_connect('sql311.epizy.com', 'epiz_33459705', 'hvZgW4p2guu7');
 		if (!$link) {
     	die('Could not connect: ' . mysql_error());
 		}
 
-$db_selected = mysql_select_db('pre_registration', $link);
+$db_selected = mysql_select_db('epiz_33459705_cbtexam_db', $link);
 if (!$db_selected) {
    die ('Can\'t open form submit database : ' . mysql_error());
 }
